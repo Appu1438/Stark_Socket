fb9fe7048011c8d0cb4a3f095a96aa651866e25a
adithyan s kumar <adithyans1438@gmail.com>
2025-09-15 01:25:12 +0530

    test api function

--- server.js
+++ server.js
@@ -210,6 +210,13 @@ const findNearbyDrivers = (userLat, userLon) => {
         }));
 };
 
+app.get('/', (req, res) => {
+    res.status(200).json({
+        succcess: true,
+        message: "API is working",
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
